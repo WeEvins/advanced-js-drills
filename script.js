ea9a9192078e1b3cc92d1514e92f4c521719531a
Warryn Evins <W.Hatmaker@yahoo.com>
2018-06-07 12:28:30 -0500

    second part of lab added

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 name = 'Warryn';
-let name;
+var name;
 console.log(name);
 
+setName();
+function setName() {
+    var name = 'Warryn';
+    console.log(name);
+}
+
+// here is the second part of the lab
+
+let fruits = ['banana', 'apple', 'orange']
+let favFruit;
+
+function printFruits(){
+    favFruit = fruits[0];
+    console.log(fruits[1]);
+}
+
+function printFaveFruit(){
+    console.log(favFruit);
+    
+}
+
+printFruits();
+printFaveFruit();
